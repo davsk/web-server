d17c5c08f31df43a1955ba0622396ce0992b730d
David Skinner <skinner.david@gmail.com>
2018-08-29 04:55:17 -0500

    initial updates
    Nothing tested

---
+++ cmd/davsk_web_server/davsk_web_server.go
@@ -0,0 +1,28 @@
+// /////////////////////////////////////////////////////////////
+// 'davsk_web_srv.go'                                          /
+//                                                             /
+// Copyright (c) 2018 Davsk℠. All Rights Reserved.             /
+// Use of this source code is governed by an ISC License (ISC) /
+// that can be found in the LICENSE file.                      /
+//                                                             /
+// by David Skinner                                            /
+// on July 20, 2018                                            /
+// for Davsk℠ Universe 4.0 project gbase                       /
+//                                                             /
+// /////////////////////////////////////////////////////////////
+
+// Command davsk_web_srv is an http server.
+package main
+
+import (
+	"davsk.net/gbase/pkg/nothing"
+	"davsk.net/gbase/pkg/svc/gameserver"
+	"github.com/goinggo/tracelog"
+)
+
+func main() {
+	tracelog.Start(tracelog.LevelTrace)
+	webserver.MustStart()
+	nothing.MustDo()
+	tracelog.Stop()
+}
